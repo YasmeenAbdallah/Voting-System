3f5905fcb9010ede4c5134d4f9e6eac0bcf3c0ce
yassmeena <zulema11898@gmail.com>
2020-08-06 14:59:01 +0200

    edit the contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,6 +6,9 @@ contract Election {
         string name;
         uint voteCount;
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
      // Read/write Candidates
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
@@ -22,6 +25,19 @@ contract Election {
     function addCandidate (string memory  _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+     function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 
 
